f14961b166a4bac4ea5776456cbdc82ff753e387
LazyScriptTurtle <162825856+LazyScriptTurtle@users.noreply.github.com>
2025-04-13 14:02:08 +0200

    Poprawa logiki

--- CoreController.psm1
+++ CoreController.psm1
@@ -1,113 +1,58 @@
-function Import-BasicAVConfig {
-    param (
-        [string]$ConfigPath = ".\config.json"
-    )
-
-    if (-Not (Test-Path -Path $ConfigPath)) {
-        Write-Host "[-] Error: Configuration file does not exist." -ForegroundColor Red
-        return $null
-    }
-
-    try {
-        $config = Get-Content -Raw -Path $ConfigPath | ConvertFrom-Json
-    } catch {
-        Write-Host "[-] Error: Failed to read or parse the JSON file." -ForegroundColor Red
-        return $null
-    }
-
-
-    function Expand-EnvInArray($array) {
-        return $array | ForEach-Object { [Environment]::ExpandEnvironmentVariables($_) }
-    }
-
-
-    if ($config.RealTimeMonitor.WatchedPaths) {
-        $config.RealTimeMonitor.WatchedPaths = Expand-EnvInArray $config.RealTimeMonitor.WatchedPaths
-    }
-    if ($config.FileHoneypot.TargetFolders) {
-        $config.FileHoneypot.TargetFolders = Expand-EnvInArray $config.FileHoneypot.TargetFolders
-    }
-    Write-Host "[+] Configuration loaded correctly " -ForegroundColor Green
-}
-
-function Test-Administrator  
-{  
-    $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-
-    if($isAdmin -eq $false)
-    {
-        Write-Host "[-] Strongly recommend running the program as Administrator. " -ForegroundColor Red
-        Write-Host "[!] Do you want to continue Y/N (Yes/No) ? " -ForegroundColor Blue -NoNewline
-        do {
-        $Choice = Read-Host
-        if($Choice.ToUpper() -eq "Y")
-        {
-            continue
-        }
-        elseif ($Choice.ToUpper() -eq "N" )
-        {
-            exit
-        }
-        else
-        {
-            Write-Host "[!] Error: Wrong Value " -ForegroundColor Yellow
-            Write-Host "[!] Do you want to continue Y/N (Yes/No) ? " -ForegroundColor Blue -NoNewline
-        }
-    }
-    while ($true)
-        
-    }
-    else {
-        Write-Host "[+] Priviledge OK " -ForegroundColor Green
-    }
-}
 
 function Import-BasicAVModules {
 
     try {
-    $AllModule = Get-ChildItem -Path ".\Modules\" -Recurse -Filter "*.psm1" 
-    foreach ( $Module in $AllModule)
-    {
-        Write-Host "[/] Module import $($Module.FullName) $Module ... " -ForegroundColor Cyan
-        $ModulePath = $Module.FullName  
-        Import-Module $ModulePath -Force -Global
-        $loaded = Get-Module | Where-Object { $_.Path -eq $Module.FullName }
-        if ($null -ne $loaded)
-        {
-            Write-Host "[+] Module $($Module.FullName) was imported. " -ForegroundColor Green 
+        $AllModule = Get-ChildItem -Path ".\Modules\" -Recurse -Filter "*.psm1" 
+        foreach ( $Module in $AllModule) {
+            Write-Host "[/] Module import $($Module.FullName) $Module ... " -ForegroundColor Cyan
+            $ModulePath = $Module.FullName  
+            Import-Module $ModulePath -Force -Global
+            $loaded = Get-Module | Where-Object { $_.Path -eq $Module.FullName }
+            if ($null -ne $loaded) {
+                Write-Host "[+] Module $($Module.FullName) was imported. " -ForegroundColor Green 
+            }
+            else {
+                Write-Host "[-] Error module $($Module.FullName) was not imported correctly" -ForegroundColor Red
+            }
         }
-        else {
-            Write-Host "[-] Error module $($Module.FullName) was not imported correctly" -ForegroundColor Red
-        }
-    }
 
-}
+    }
     catch {
-     Write-Host "[-] Listing Module problem Error: $_ " -ForegroundColor Red
+        Write-Host "[-] Listing Module problem Error: $_ " -ForegroundColor Red
     }
 
 }
 
 function Start-BasicAV {
 
-    Write-Host "BasicAV running ..." -ForegroundColor Green
-    sleep -Seconds 3
-    Write-Host "Chceking Administrators Priviledges ..." -ForegroundColor Green
-    Test-Administrator
-    Sleep -Seconds 1
-    Write-Host "Importing configuration ..." -ForegroundColor Green
-    Import-BasicAVConfig
-    sleep -Seconds 1
-    Write-Host "Configure modules ..." -ForegroundColor Green
+    Write-Host "BasicAV running ..." -ForegroundColor Cyan
+    sleep -Seconds 2
+    Write-Host "Configure modules ..." -ForegroundColor Cyan
     Import-BasicAVModules
     sleep -Seconds 1
+    Write-Host "Chceking Administrators Priviledges ..." -ForegroundColor Cyan
+    $CheckPriv = Test-Administrator
+    Sleep -Seconds 1
+    Write-Host "Importing configuration ..." -ForegroundColor Cyan
+    Import-BasicAVConfig -Path ".\config.json"
+    sleep -Seconds 1
+    Write-Host "Validate Configuration ..." -ForegroundColor Cyan
+   # Configuration-Validate
+    sleep -Seconds 1
+    Write-Host "Configure Quarantine Folder ..." -ForegroundColor Cyan
+    if ($CheckPriv -ne $true)
+    {
+        Write-Host "[-] Insufficient permissions, please run the program again as Administrator " -ForegroundColor Red
+        exit
+    }else {
+        New-QuarantineFolder -Path ".\Quarantine"
+        sleep -Seconds 1
+    }
     Write-Host ""
-    Write-Host "Everything ready " -ForegroundColor Green
-    Write-Host "Press key to start menu " -ForegroundColor Blue -NoNewline
+    Write-Host "Press 'Enter' to start menu " -ForegroundColor Blue -NoNewline
     $Interact = Read-Host 
     $Interact
-    if ($Interact -ne $null)
-    {
+    if ($Interact -ne $null) {
         Get-MainMenu
     }
     else {
