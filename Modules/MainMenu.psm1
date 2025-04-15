746987160e1d6240dd4e5a7756733221a179939e
LazyScriptTurtle <162825856+LazyScriptTurtle@users.noreply.github.com>
2025-04-15 19:54:07 +0200

    2 features added “4” in progress (not yet working properly)

--- Modules/MainMenu.psm1
+++ Modules/MainMenu.psm1
@@ -1,52 +1,80 @@
-function Get-MainMenu{
+function Get-MainMenu {
     Start-Sleep -Seconds 2
-Clear-Host
+    Clear-Host
 
-do {
-Clear-Host
-Write-Host '  //===============================\\ '
-Write-Host ' //  '-NoNewline
-Write-Host '        Basic Antivirus' -ForegroundColor Green -NoNewline
-Write-Host '        \\'
-Write-Host '//===================================\\'
-Write-Host '|| 1. Full Scan      | 6.  X         ||' 
-Write-Host '||                   |               ||'
-Write-Host '|| 2. Custom Scan    | 7.  X         ||' 
-Write-Host '||                   |               ||'
-Write-Host '|| 3. Fast Scan      | 8.  X         ||' 
-Write-Host '||                   |               ||'
-Write-Host '|| 4. Malware Search | 9.  X         ||' 
-Write-Host '||                   |               ||'
-Write-Host '|| 5. Magic Numbers  | 10. Help      ||' 
-Write-Host '||                   |               ||'
-Write-Host '\\===================================//'
-$choice = Read-Host ' Choose your option: '
+    do {
+        Clear-Host
+        Write-Host '  //===============================\\ '
+        Write-Host ' //  '-NoNewline
+        Write-Host '        Basic Antivirus' -ForegroundColor Green -NoNewline
+        Write-Host '        \\'
+        Write-Host '//===================================\\'
+        Write-Host '|| 1. Full Scan      | 6.  X         ||' 
+        Write-Host '||                   |               ||'
+        Write-Host '|| 2. Custom Scan    | 7.  X         ||' 
+        Write-Host '||                   |               ||'
+        Write-Host '|| 3. Fast Scan      | 8.  X         ||' 
+        Write-Host '||                   |               ||'
+        Write-Host '|| 4. Malware Search | 9.  X         ||' 
+        Write-Host '||                   |               ||'
+        Write-Host '|| 5. Magic Numbers  | 10. Help      ||' 
+        Write-Host '||                   |               ||'
+        Write-Host '\\===================================//'
+        $choice = Read-Host ' Choose your option: '
 
-switch ($choice) {
-    1 {
-
-    }
+        switch ($choice) {
+            1 {
+                Start-ScanWithC -Path .\Modules\FilesScanner.exe
+                if (-not (Test-Path -Path .\scan.txt))
+                {
+                    Write-Host "[-] Error Scan Interrupted " -ForegroundColor Red
+                }else{Write-Host "[+] Scan End Successfully "  -ForegroundColor Green
+                Pause
+            }
+                
+                
+            }
     
-    2 {
+            2 {
 
-    }
+            }
     
-    3 {
+            3 {
 
-    }
+            }
     
-    4 {
+            4 {
 
-    }
+            }
+
+            5 {
+
+            }
+
+            6 {
+
+            }
+
+            7 {
+
+            }
+
+            8 {
+
+            }
+
+            9 {
+
+            }
     
-    10 {
+            10 {
 
 
-    }
+            }
 
-    Default {
-        Write-Host "Invalid selection, please try again." -ForegroundColor Red
-    }
-}
- } while ($true)
+            Default {
+                Write-Host "Invalid selection, please try again." -ForegroundColor Red
+            }
+        }
+    } while ($true)
 }
